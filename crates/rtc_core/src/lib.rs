ef58a3e04b22f236c31b31b7bba4f196c0eb63a8
Avery Parr <averyparr@gmail.com>
2025-12-29 22:13:52 -0500

    Move to exterior crate

--- crates/rtc_core/src/lib.rs
+++ crates/rtc_core/src/lib.rs
@@ -1,60 +1,7 @@
-mod codegen;
-mod primitives;
-mod ty;
-mod val;
+pub mod codegen;
+pub mod primitives;
+pub mod ty;
+pub mod val;
 
-use inkwell::{OptimizationLevel, targets::FileType};
-
-use crate::{
-    codegen::{
-        func_with_args::create_func,
-        target::{PTXOptions, SM, TargetMachine},
-    },
-    primitives::{MutOps, RefOps},
-    ty::{F32, F64, M, R, Void},
-    val::C,
-};
-
-fn main() {
-    let func = create_func::<(R<F32>, R<F32>, M<F64>), Void>();
-    let (a_ptr, b_ptr, mut c_ptr) = func.get_args();
-
-    let a = a_ptr.load();
-    let b = b_ptr.load();
-    let c = a + b;
-    let mut c_stor = c.with_storage();
-    let mut c_mut = c_stor.get_mut();
-    let const_float = func.const_val(5.0);
-    c_mut.store(b + c + const_float);
-
-    let c_f64 = a * (c_stor.get() + b);
-    let e = (c_f64 + C(5.0)).float_cast::<F64>();
-    c_ptr.store(e);
-
-    let to_jit = func.finalize();
-    let out = to_jit.compile(
-        TargetMachine::PTX(PTXOptions { sm: SM::SM80 }),
-        OptimizationLevel::Aggressive,
-        FileType::Assembly,
-    );
-    let s = str::from_utf8(&out).expect("Should be valid UTF-8");
-    println!("{}", s);
-}
-
-// mod test {
-//     use inkwell::{OptimizationLevel, targets::FileType};
-
-//     use crate::{
-//         codegen::{
-//             func_with_args::create_func,
-//             target::{PTXOptions, SM, TargetMachine},
-//         },
-//         ty::{F32, F64, M, R, Void},
-//         val::C,
-//     };
-
-//     use super::*;
-
-//     #[test]
-
-// }
+pub use inkwell::OptimizationLevel;
+pub use inkwell::targets::FileType;
