bd345a755806abf6c106fa54c05b148387a5b097
Avery Parr <averyparr@gmail.com>
2026-02-27 16:06:52 -0500

    LLVM optimizations also mostly work!

--- crates/rtc_types/src/lib.rs
+++ crates/rtc_types/src/lib.rs
@@ -17,21 +17,14 @@ type FlAlt = F32;
 const VEC_LEN: usize = 4;
 
 pub fn test_inner() {
-    let kernel = new_ptx_kernel::<(Global<R<&Fl>>, Global<R<&U32>>, Global<M<&mut Fl>>)>();
+    let kernel = new_ptx_kernel::<(Global<R<&Fl>>, Global<R<&U32>>, Global<M<&mut U32>>)>();
     kernel.use_fast_math();
     let (a, b, mut c) = kernel.get_args();
 
-    let mut a = a.load();
     let max = b.load();
-    let mut a_prime = a.clone();
-    let mut a_prime = a_prime.as_mut();
-    for _ in Loop::new(max.const_like(4)..10 * max) {
-        let mut a_mut = a.as_mut();
-        a_mut.store(a_mut.load() * a_mut.load());
-        a_prime.store(a_mut.load() * c.load());
+    for idx in Loop::new(max.const_like(4)..10 * max) {
+        c.store(idx);
     }
-
-    c.store(a_prime.load());
 
     println!("{}", kernel.finalize().compile_asm_quickly(SM::SM90));
     // assert!(false);
