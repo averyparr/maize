5d6ddceca0e90e97190054dd3b6ff555c0530d51
Avery Parr <averyparr@gmail.com>
2026-03-05 17:30:37 -0500

    had missed a few pre-SSA-return things in kernel assert

--- crates/rtc_types/src/val/assert.rs
+++ crates/rtc_types/src/val/assert.rs
@@ -1,14 +1,14 @@
 #[macro_export]
 macro_rules! kernel_assert {
     ($cond: expr, $msg: literal) => {{
-        let cond: $crate::val::Val<'_, Bool> = $cond.copy();
+        let cond: $crate::val::Val<'_, $crate::ty::Bool> = $cond;
         fn f() {}
         // ::[...]::above_fn_name::f
         let mut name = ::core::any::type_name_of_val(&f);
         name = &name[..name.len() - 3];
         cond.cx()
             .intrinsics()
-            .assert(cond.copy(), $msg, file!(), line!(), name)
+            .assert(cond, $msg, file!(), line!(), name)
     }};
     ($cond: expr) => {
         kernel_assert!($cond, "Assertion failed: ")
